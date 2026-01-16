33de5a81039bfbccccb748cd7ce719c9ed39093c
Shai Vure <shaivure@gmail.com>
2026-01-16 23:01:11 +0000

    fixed build issue

--- app/src/main/java/com/hyliankid14/bbcradioplayer/PodcastsFragment.kt
+++ app/src/main/java/com/hyliankid14/bbcradioplayer/PodcastsFragment.kt
@@ -19,6 +19,8 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.Locale
